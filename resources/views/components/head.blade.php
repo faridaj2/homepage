8bd7f3a33f0272c4f90ffdecbc567415c3ce27d1
faridaj2 <ajfarid6@gmail.com>
2024-09-17 13:51:47 +0700

    fix css

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,5 +1,6 @@
-{{-- <link rel="stylesheet" href="/build/assets/app-65871c56.css"> --}}
+<link rel="stylesheet" href="/build/assets/app-391b0b63.css">
+<script src="/build/assets/app-ddee773b.js"></script>
 {{-- <link rel="stylesheet" href="http://localhost:5173/resources/css/app.css"> --}}
-@vite('resources/css/app.css')
-@vite('resources/js/app.js')
+{{-- @vite('resources/css/app.css') --}}
+{{-- @vite('resources/js/app.js') --}}
 {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}

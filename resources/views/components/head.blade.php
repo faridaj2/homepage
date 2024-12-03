40d8e3bb811dee694122bb5c7965c2fb270d2872
faridaj2 <ajfarid6@gmail.com>
2024-12-04 00:00:36 +0700

    fix share button

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/build/assets/app-391b0b63.css">
+<link rel="stylesheet" href="/build/assets/app-4f86e792.css">
 <script src="/build/assets/app-ddee773b.js"></script>
 {{-- <link rel="stylesheet" href="http://localhost:5173/resources/css/app.css"> --}}
 {{-- @vite('resources/css/app.css') --}}

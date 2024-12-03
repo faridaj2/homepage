c5741313e5acf1091ed8cc978115e1d9834477bf
faridaj2 <ajfarid6@gmail.com>
2024-12-04 00:46:11 +0700

    fix share button

--- resources/views/components/dashboard/grid/grid.blade.php
+++ resources/views/components/dashboard/grid/grid.blade.php
@@ -17,7 +17,13 @@
         </a>
         <a href="/dashboard/{{ $url }}/{{ $item->id }}/edit"
             class="text-black bg-gray-200 w-10 h-10 rounded-full flex items-center justify-center shadow-md hover:bg-gray-300 transition-all">
-            <i data-lucide="pencil"></i>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-pencil">
+                <path
+                    d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                <path d="m15 5 4 4" />
+            </svg>
         </a>
         <button
             class="text-black bg-gray-200 w-10 h-10 rounded-full flex items-center justify-center shadow-md hover:bg-gray-300 transition-all"

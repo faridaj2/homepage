51945d4ec63eb2ae807861758a42786bec11d3ff
faridaj2 <ajfarid6@gmail.com>
2024-12-11 15:56:16 +0700

    fix css

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/build/assets/app-e70d6196.css ">
+<link rel="stylesheet" href="/build/assets/app-3d1151b0.css">
 <script src="/build/assets/app-ddee773b.js"></script>
 <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
 {{-- <link rel="stylesheet" href="http://localhost:5173/resources/css/app.css"> --}}

f5f7abbcc77795e83ffdf5617dfba6c18bf3090e
faridaj2 <ajfarid6@gmail.com>
2024-12-19 20:30:20 +0700

    fix seo

--- app/Models/berita.php
+++ app/Models/berita.php
@@ -9,4 +9,13 @@ class berita extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+    protected function getDynamicSEOData(): SEOData
+{
+    return new SEOData(
+        title: $this->title,
+        description: $this->excerpt,
+        author: "Admin",
+        image: "https://darussalam2.com/storage/file/" . $this->image_url;
+    );
+}
 }

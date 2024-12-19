98647c0b2e5bed853a67cc72250e18c6e2e3a409
faridaj2 <ajfarid6@gmail.com>
2024-12-20 00:08:22 +0700

    fix seo

--- app/Models/berita.php
+++ app/Models/berita.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use RalphJSmit\Laravel\SEO\Support\HasSEO;
 
 class berita extends Model
 {
     use HasFactory;
+    use HasSEO;
     protected $guarded = ['id'];
     protected function getDynamicSEOData(): SEOData
     {

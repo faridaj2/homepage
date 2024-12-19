8e8012c0097dea4272383287523e040153e9728b
faridaj2 <ajfarid6@gmail.com>
2024-12-20 01:38:16 +0700

    fix seo

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use RalphJSmit\Laravel\SEO\Support\HasSEO;
+use RalphJSmit\Laravel\SEO\Support\SEOData;
 
 class Berita extends Model
 {

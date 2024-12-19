782b34441a51323308a49389dc02568545bea590
faridaj2 <ajfarid6@gmail.com>
2024-12-20 01:13:57 +0700

    fix seo

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- <title>Home - Pondok Pesantren Darussalam Blokagung 2</title> --}}
-    {!! seo($berital) ? seo($berita) : seo() !!}
+    {!! seo($berita) ? seo($berita) : seo() !!}
 
     {{-- Css & Js --}}
 

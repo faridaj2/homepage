45efbed6a3b8f1cd94a1082c7ccf838d038972c1
faridaj2 <ajfarid6@gmail.com>
2026-07-09 14:06:27 +0700

    test

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,2 +1,4 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="/build/assets/app-ddee773b.js"></script>
+<style link="/build/assets/app-3d1151b0.css"></style>
 <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>

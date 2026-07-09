384b2ae2619a549d1e392d55e6d3f9c2f31ef3cf
faridaj2 <ajfarid6@gmail.com>
2026-07-09 14:01:13 +0700

    fix: use @vite() for asset loading instead of hardcoded hashes

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,7 +1,2 @@
-<link rel="stylesheet" href="/build/assets/app-3d1151b0.css">
-<script src="/build/assets/app-ddee773b.js"></script>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
-{{-- <link rel="stylesheet" href="http://localhost:5173/resources/css/app.css"> --}}
-{{-- @vite('resources/css/app.css') --}}
-{{-- @vite('resources/js/app.js') --}}
-{{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
